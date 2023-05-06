8e2dc2aff73d2c936d86fc79e5a0e166b49a1e73
XasanAbdi <xasancabdinasax@gmail.com>
2023-05-06 11:07:35 -0700

    ecommerce

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,10 +3,10 @@ import generateToken from "../utilities/generatetoken.js";
 
 export const register=async(req,res)=>{
     const {name,email,password,address,phone}=req.body;
-    const userExits=Users.findOne({email})
+    const userExits=await Users.findOne({email})
    if (userExits) {
     res.status(400).json({message:'User allready Exists'})
-   }
+   }else{
     const user =Users.create({
         name,email,password,address,phone
     }) 
@@ -23,15 +23,18 @@ export const register=async(req,res)=>{
 
         })
     
-    }   else{
+    } else{
         res.status(401).json({message:'invalid user data'});
     }
+
+   }
+      
  
     
 }
 export const login=async(req,res)=>{
     const {email,password} = req.body;
-    const user =Users.findOne({email});
+    const user =await Users.findOne({email});
     if (user && password==user.password) {
         res.status(201).json({
             _id:user._id,
